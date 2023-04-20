994ccc15213e26a3d936ee925aedcd3f90997043
Sujuforny <pengny2002@gmail.com>
2023-04-20 08:35:20 +0700

    fixed eorre loading

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import './App.css';
 import ProductsList from './components/ProductsList';
 import Placeholder from 'react-bootstrap/Placeholder';
-import Card from 'react-bootstrap/Card';
- 
-
 function App() {
 
   return (
@@ -11,20 +8,6 @@ function App() {
     <div className='all-products'>
     <ProductsList/>
     </div>
-    <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src=" " />
-        <Card.Body>
-          <Placeholder as={Card.Title} animation="glow">
-            <Placeholder xs={6} size="xs" />
-          </Placeholder>
-          <Placeholder as={Card.Text} animation="glow">
-            <Placeholder xs={7} /> <Placeholder xs={4} /> <Placeholder xs={4} />{' '}
-            <Placeholder xs={6} /> <Placeholder xs={8} />
-          </Placeholder>
-          <Placeholder.Button variant="primary" xs={6} />
-        </Card.Body>
-      </Card>
-    
     </>
   );
 }
--- src/components/ProductsList.js
+++ src/components/ProductsList.js
@@ -1,13 +1,37 @@
 import React, { useState, useEffect } from 'react'
 import Card from 'react-bootstrap/Card';
 import Buttons from './Buttons';
+import { Placeholder } from 'react-bootstrap';
 
 export default function ProductsList() {
+  let placeholder = [];
     const [data,setdata]=useState([]);
     const product = document.querySelector('.product');
     useEffect(()=>{
         fetch("https://api.escuelajs.co/api/v1/products").then(item=> item.json()).then(data => setdata(data))
     },[])
+    // loop placeholder
+  
+  for (let i = 0; i < 50; i++) {
+    placeholder.push(
+      <Card className="d-inline-flex m-3" style={{ width: "18rem" }}>
+        <Card.Img
+          variant="top"
+          src="https://cdn.dribbble.com/users/594619/screenshots/16710203/media/a4a970dc53a3aa851927f338751dea2d.gif"
+        />
+        <Card.Body>
+          <Placeholder as={Card.Title} animation="glow">
+            <Placeholder xs={6} />
+          </Placeholder>
+          <Placeholder as={Card.Text} animation="glow">
+            <Placeholder xs={7} /> <Placeholder xs={4} />
+            <Placeholder xs={4} /> <Placeholder xs={6} /> <Placeholder xs={8} />
+          </Placeholder>
+          <Placeholder.Button variant="primary" xs={6} />
+        </Card.Body>
+      </Card>
+    );
+  }
   return (
       <>
       {data.map((product) => (
@@ -23,6 +47,7 @@ export default function ProductsList() {
               </Card.Body>
         </Card>
         ))}
+        {placeholder}
       </>
   )
 }
